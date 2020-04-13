5ddc1519aba4c19be9619f0c5efe5a22eaa341e1
afans4 <61768816+afans4@users.noreply.github.com>
2020-04-13 16:10:41 +0800

    Create Dualedge.v

---
+++ HDLBIT_partice/Dualedge.v
@@ -0,0 +1,13 @@
+// 双边沿采样
+module top_module (
+    input clk,
+    input d,
+    output q
+);
+    reg p,n;
+    always @ (posedge clk)
+        p<=d^n;
+    always @ (negedge clk)
+        n<=d^p;
+    assign q=p^n;
+endmodule
